e487469eaac95ba6c43300965a858c8706c7bbda
Varsha Iyer <varsha.b.iyer@gmail.com>
2019-05-09 15:15:03 -0400

    workshopp

--- src/actions/operations.js
+++ src/actions/operations.js
@@ -6,3 +6,44 @@ export const AddStar = gql``;
 export const RemoveStar = gql``;
 
 export const GetRepos = gql``;
+query listRepos($queryString:String!){
+    search(query: $queryString, type: USER, first:1) {
+      edges {
+        node {
+          ... on User {
+            name
+            repositories(first: 20) {
+              edges {
+                node {
+                  name
+                  id
+                  viewerHasStarred
+                  createdAt
+                  description
+                  url
+                  defaultBranchRef {
+                    target {
+                      ... on Commit {
+                        history(first:10) {
+                          totalCount
+                          edges {
+                            node {
+                              ... on Commit {
+                                committedDate
+                                message
+                              }
+                            }
+                          }
+                        }
+                      }
+                    }
+                  }
+                }
+              }
+            }
+          }
+        }
+      }
+    }
+  }
+  
